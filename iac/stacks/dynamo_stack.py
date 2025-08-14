028b020f9bf586e982cfc7944756fe3f44cfdd04
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-14 12:43:39 -0300

    adding good practices do dynamo stack

--- iac/stacks/dynamo_stack.py
+++ iac/stacks/dynamo_stack.py
@@ -3,9 +3,11 @@ from aws_cdk import (
     aws_dynamodb as dynamodb, RemovalPolicy,
 )
 from constructs import Construct
+import os
 
 
 class DynamoStack(Construct):
+    
     table: dynamodb.Table
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -13,17 +15,19 @@ class DynamoStack(Construct):
 
         self.github_ref_name = os.environ.get("GITHUB_REF_NAME")
         self.stack_name = os.environ.get("STACK_NAME")
-
         stage = ''
-        if 'prod' in self.github_ref:
-            stage = 'PROD'
-        elif 'homolog' in self.github_ref:
+        
+        if 'prod' in self.github_ref_name.lower():
+            stage = 'PROD'            
+        elif 'homolog' in self.github_ref_name.lower():
             stage = 'HOMOLOG'
         else:
             stage = 'DEV'
+            
+        removal_policy = RemovalPolicy.RETAIN if stage=="PROD" else RemovalPolicy.DESTROY
 
         self.table = dynamodb.Table(
-            self, "PortalEntidades_member_Table",
+            self, "PortalEntidades_MemberTable",
             partition_key=dynamodb.Attribute(
                 name="PK",
                 type=dynamodb.AttributeType.STRING
@@ -33,7 +37,10 @@ class DynamoStack(Construct):
                 type=dynamodb.AttributeType.STRING
             ), 
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=RemovalPolicy.DESTROY
+            removal_policy=removal_policy,
+            
+            table_name=f"PortalEntidadesMemberTable-{stage.upper()}",
+            point_in_time_recovery=True if stage=="PROD" else False
         )
         
     
