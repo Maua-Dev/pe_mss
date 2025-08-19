a0a2d8b7cb4261aafa95dfe3a2c94344b55e088e
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-18 23:06:00 -0300

    minor change

--- src/shared/infra/repositories/user_repository_mock.py
+++ src/shared/infra/repositories/user_repository_mock.py
@@ -7,7 +7,7 @@ from src.shared.helpers.errors.usecase_errors import NoItemsFound
 from src.shared.domain.enums.course_enum import COURSE
 from src.shared.domain.enums.organization_enum import ORGANIZATION
 from src.shared.domain.enums.role_enum import ROLE
-
+import uuid
 
 class UserRepositoryMock(IUserRepository):
     users: List[User]
@@ -17,7 +17,7 @@ class UserRepositoryMock(IUserRepository):
             User(name="Guilherme",email="25.00178-5@maua.br", ra="25.00178-5", state=STATE.PENDING, role=ROLE.USER, user_id="550e8400-e29b-41d4-a716-446655440000"),
             User(name="João",email="21.00678-2@maua.br", ra="24.00678-2", state=STATE.APPROVED, role=ROLE.ADM, course=COURSE.CIC, year=4, organization=ORGANIZATION.DEV, user_id="550e8400-e29b-41d4-a716-446655440001"),
             User(name="Heitor", email="21.00453-7@maua.br", ra="21.00453-7", state=STATE.APPROVED, role=ROLE.USER, course=COURSE.ECM, year=4, organization=ORGANIZATION.DEV, user_id="550e8400-e29b-41d4-a716-446655440002"),
-            User(name="Bruno", email="21.00458-7@maua.br", ra="21.00458-7", state=STATE.REJECTED, role=ROLE.PRESIDENT, course=COURSE.EET, year=1, organization=ORGANIZATION.GUARDIAN, user_id="550e8400-e29b-41d4-a716-446655440003")
+            User(name="Bruno", email="21.00458-7@maua.br", ra="21.00458-7", state=STATE.REJECTED, role=ROLE.PRESIDENT, course=COURSE.EET, year=1, organization=ORGANIZATION.GUARDIAN, user_id=str(uuid.uuid4())) # testing if uuid4 is a valid id for user entity
         ]
     
     def get_user(self, user_id: int) -> User:
