1dfe771689a32641d3713a880f01b40891ff73f5
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-09-27 20:25:42 -0300

    fixing layer directory name + adding better visualization

--- iac/contructs/lambda_construct.py
+++ iac/contructs/lambda_construct.py
@@ -9,33 +9,51 @@ from aws_cdk.aws_apigateway import Resource, LambdaIntegration
 
 class LambdaConstruct(Construct):
    
-    def create_lambda_api_gateway_integration(self, module_name: str, method: str, mss_student_api_resource: Resource,
-                                              environment_variables: dict = {"STAGE": "TEST"}, authorizer = None):
+    def create_lambda_api_gateway_integration(
+        self, 
+        module_name: str, 
+        method: str, 
+        mss_student_api_resource: Resource,
+        environment_variables: dict = {"STAGE": "TEST"}, 
+        authorizer = None
+    ):
+        
         function = lambda_.Function(
             self, module_name.title(),
             code=lambda_.Code.from_asset(f"../src/modules/{module_name}"),
             handler=f"app.{module_name}_presenter.lambda_handler",
-            runtime=lambda_.Runtime.PYTHON_3_9,
-            layers=[self.lambda_layer, self.lambda_power_tools],
+            runtime=lambda_.Runtime.PYTHON_3_11,
+            layers=[self.lambda_layer],
             environment=environment_variables,
             timeout=Duration.seconds(15)
         )
 
-        mss_student_api_resource.add_resource(module_name.replace("_", "-")).add_method(method,
-                                                                                        integration=LambdaIntegration(
-                                                                                            function))
+        mss_student_api_resource.add_resource(
+            module_name.replace("_", "-")).add_method(
+                method,
+                integration=LambdaIntegration(function)
+            )
 
         return function
 
-    def __init__(self, scope: Construct, api_gateway_resource: Resource, environment_variables: dict) -> None:
+    def __init__(
+        self, 
+        scope: Construct, 
+        api_gateway_resource: Resource, 
+        environment_variables: dict
+    ) -> None:
+        
         super().__init__(scope, "PortalEntidades_Lambdas")
 
-        self.lambda_layer = lambda_.LayerVersion(self, "PortalEntidades_Layer",
-                                                 code=lambda_.Code.from_asset("./lambda_layer_out_temp"),
-                                                 compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
-                                                 )
-
-        self.lambda_power_tools = lambda_.LayerVersion.from_layer_version_arn(self, "Lambda_Power_Tools", layer_version_arn="arn:aws:lambda:us-east-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:22")
+        self.lambda_layer = lambda_.LayerVersion(
+            self, 
+            "PortalEntidades_Layer",
+            # Esse nome precisa estar correspondendo exatamente ao diretório criado pelo script adjust_layer_directory.py
+            # Mais especificamente na variável BUILD_DIRECTORY
+            # ./{BUILD_DIRECTORY}
+            code=lambda_.Code.from_asset("./build"), 
+            compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
+        )
 
         self.update_user_function = self.create_lambda_api_gateway_integration(
             module_name="update_user",
@@ -44,6 +62,9 @@ class LambdaConstruct(Construct):
             environment_variables=environment_variables
         )
 
-        self.functions_that_need_db_access = [self.update_user_function]
+        self.functions_that_need_db_access = [
+            self.update_user_function
+        ]
         
-        self.functions_that_need_s3_permissions = []
+        self.functions_that_need_s3_permissions = [
+        ]
