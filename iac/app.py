86c40c43b2c2512fa63d17d931517d8a3b7441e8
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-09-28 10:56:22 -0300

    fixing construct_id name

--- iac/app.py
+++ iac/app.py
@@ -40,7 +40,12 @@ tags = {
     'owner': 'DevCommunity'
 }
 
-IacStack(app, stack_name=stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
+IacStack(
+    scope=app, 
+    construct_id=stack_name, 
+    env=cdk.Environment(account=aws_account_id, region=aws_region), 
+    tags=tags
+)
 
 
 app.synth()
