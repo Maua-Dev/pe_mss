9ba0877254c49942d775a90420d69ebdebe3b450
Matelz <49626198+Matelz@users.noreply.github.com>
2025-10-02 07:55:46 -0300

    fix: import missing class

--- src/modules/delete_user/app/delete_user_controller.py
+++ src/modules/delete_user/app/delete_user_controller.py
@@ -4,7 +4,7 @@ from .delete_user_usecase import DeleteUserUsecase
 from .delete_user_viewmodel import DeleteUserViewmodel
 from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, ForbiddenAction
 from src.shared.helpers.external_interfaces.http_codes import OK, NotFound, BadRequest, InternalServerError
 
 
