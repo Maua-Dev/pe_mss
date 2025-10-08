0874301ec3b6bf277cc93128d06df8209a5de0bd
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-10-08 13:19:47 -0300

    attempting arn login in lambda construct

--- iac/components/lambda_construct.py
+++ iac/components/lambda_construct.py
@@ -1,7 +1,8 @@
 import os
 from aws_cdk import (
     aws_lambda as lambda_,
-    NestedStack, Duration
+    NestedStack, Duration,
+    aws_apigateway as apigw
 )
 from constructs import Construct
 from aws_cdk.aws_apigateway import Resource, LambdaIntegration
@@ -47,6 +48,23 @@ class LambdaConstruct(Construct):
         #pega um exemplo do reservation_api, la tem um authorizer nesse mesmo arquivo
         
         super().__init__(scope, "PortalEntidades_Lambdas")
+        
+        authorizer_lambda = lambda_.Function(
+            self, "AuthorizerPEMssLambda",
+            code=lambda_.Code.from_asset("../src/shared/authorizer"),
+            handler="graph_authorizer.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_11,
+            environment=environment_variables,
+            timeout=Duration.seconds(15)
+        )
+
+        token_authorizer_lambda = apigw.TokenAuthorizer(
+            self, "TokenAuthorizerPEMssApi",
+            handler=authorizer_lambda,
+            identity_source=apigw.IdentitySource.header("Authorization"),
+            authorizer_name="AuthorizerPEMssLambda",
+            results_cache_ttl=Duration.seconds(0)
+        )
 
         self.lambda_layer = lambda_.LayerVersion(
             self, 
@@ -54,6 +72,27 @@ class LambdaConstruct(Construct):
             # WARNING: O diretório "build" deve ser o mesmo usado em adjust_layer_direcory.py
             code=lambda_.Code.from_asset("./build"),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_11]
+        )
+        
+        self.upload_users_function = self.create_lambda_api_gateway_integration(
+            module_name="upload_users",
+            method="POST",
+            mss_student_api_resource=api_gateway_resource,
+            environment_variables=environment_variables,
+            authorizer=token_authorizer_lambda
+        )
+        
+        self.create_user_function = self.create_lambda_api_gateway_integration(
+            module_name="create_user",
+            method="POST",
+            mss_student_api_resource=api_gateway_resource,
+            environment_variables=environment_variables,
+            authorizer=token_authorizer_lambda
+        )
+        
+        allowed_arns = [self.create_user_function.function_arn]
+        authorizer_lambda.add_environment(
+            "ALLOWED_LAMBDA_ARNS", ",".join(allowed_arns)
         )
 
         self.update_user_function = self.create_lambda_api_gateway_integration(
@@ -68,4 +107,5 @@ class LambdaConstruct(Construct):
         ]
         
         self.functions_that_need_s3_permissions = [
+            self.upload_users_function
         ]
