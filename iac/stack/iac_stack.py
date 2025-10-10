fe2514b88ceff1b593c07242665851d6313bdf2d
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-10-10 09:34:37 -0300

    removing multipart form data

--- iac/stack/iac_stack.py
+++ iac/stack/iac_stack.py
@@ -29,8 +29,7 @@ class IacStack(Stack):
                                     },
                                     deploy_options={
                                         "stage_name": stage.lower()
-                                    },
-                                    binary_media_types=["multipart/form-data"]
+                                    }
                                 )
 
         api_gateway_resource = self.rest_api.root.add_resource("pe-mss", default_cors_preflight_options=
