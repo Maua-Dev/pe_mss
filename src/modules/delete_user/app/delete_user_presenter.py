2f0f293ee0276638a85bbd4595f643df86aa2d8c
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-10-14 22:22:26 -0300

    fixing presenters

--- src/modules/delete_user/app/delete_user_presenter.py
+++ src/modules/delete_user/app/delete_user_presenter.py
@@ -1,3 +1,4 @@
+import json
 from .delete_user_controller import DeleteUserController
 from .delete_user_usecase import DeleteUserUsecase
 from src.shared.environments import Environments
@@ -11,7 +12,16 @@ controller = DeleteUserController(usecase)
 def lambda_handler(event, context):
     httpRequest = LambdaHttpRequest(data=event)
 
-    httpRequest.data['user_from_authorizer']= event.get("requestContext", {}).get("authorizer", {}).get("user", None)
+    user_info_string = event.get('requestContext', {}).get('authorizer', {})
+    
+    if not user_info_string:
+        httpRequest.data['user_from_authorizer'] = None
+    elif isinstance(user_info_string, str):
+        httpRequest.data['user_from_authorizer'] = json.loads(user_info_string).get('user')
+    elif isinstance(user_info_string, dict):
+        httpRequest.data['user_from_authorizer'] = user_info_string.get('user')
+    else:
+        httpRequest.data['user_from_authorizer'] = None
     
     response = controller(httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
