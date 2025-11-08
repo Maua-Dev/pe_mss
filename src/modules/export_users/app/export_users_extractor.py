375b1a2d60ff8128f240b4ad2d37be3fb22ddf1d
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-11-08 12:40:15 -0300

    finishing export users

--- src/modules/export_users/app/export_users_extractor.py
+++ src/modules/export_users/app/export_users_extractor.py
@@ -1,12 +1,21 @@
+from src.shared.domain.enums.role_enum import ROLE
 from src.shared.domain.repositories.user_repository_interface import IUserRepository
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound
+from src.shared.helpers.external_interfaces.external_interface import IRequest
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest
 
 
 class DownloadUsersExtractor:
     def __init__(self, repo: IUserRepository):
         self.repo_user = repo
-
-    def __call__(self):
+    
+    def __call__(self, requester_user_id: str) -> dict:
+        
+        requester_user = self.repo_user.get_user(user_id=requester_user_id)
+        
+        if requester_user.role != ROLE.ADM:
+            raise ForbiddenAction("You do not have permission to access this resource.")
+        
         try:
             users= self.repo_user.get_all_user()
 
@@ -21,11 +30,11 @@ class DownloadUsersExtractor:
                 "email": user.email,
                 "ra": user.ra,
                 "role": user.role.value,
-                "state": user.state if hasattr(user.state, 'state') else user.state.value,
-                "course": user.course.value if hasattr(user.course, 'course') else user.course.value,
-                "year": user.year if hasattr(user.year, 'year') else user.year,
-                "active": user.active if hasattr(user.active, 'active') else user.active.value,
-                "organization": user.organization if hasattr(user.organization, 'organization') else user.organization.value
+                "state": user.state.value if user.state else None,
+                "course": user.course.value if user.course else None,
+                "year": user.year if user.year else None,
+                "active": user.active.value if user.active else None,
+                "organization": user.organization.value if user.organization else None
             }
 
         return users_dict
--- tests/modules/export_users/app/test_export_users_transformer.py
+++ tests/modules/export_users/app/test_export_users_transformer.py
@@ -7,7 +7,10 @@ import tempfile
 import pytest
 from src.modules.export_users.app.export_users_extractor import DownloadUsersExtractor
 from src.modules.export_users.app.export_users_transformer import DownloadUsersTransformer
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest
+from src.shared.helpers.external_interfaces.http_models import HttpRequest
 from src.shared.infra.external.postgres.datasources.postgres_datasource_tests import TestsRdsDatasource
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
 from src.shared.infra.repositories.user_repository_postgres import UserRepositoryPostgres
 
 
@@ -16,17 +19,83 @@ class TestDownloadUsers:
     
     @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipping tests in GitHub Actions environment")
     def test_export_users_transformer(self):
-        datasource= TestsRdsDatasource()
         
-        user_repo= UserRepositoryPostgres(datasource)
+        user_repo= UserRepositoryMock()
 
         extractor= DownloadUsersExtractor(repo=user_repo)
 
         transformer= DownloadUsersTransformer(extractor=extractor)
+        
+        request = HttpRequest(body={
+            'user_from_authorizer':{
+                'id': user_repo.users[1].user_id,
+                'displayName': user_repo.users[1].name,
+                'mail': user_repo.users[1].email
+            }
+        })
 
-        output_base64= transformer()
+        response = transformer(request=request)
 
-        output= base64.b64decode(output_base64)
+        output= base64.b64decode(response.body['file_base64'])
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix = ".xlsx") as temp_file:
+            temp_file.write(output)
+            temp_file_path = temp_file.name
+
+
+        if sys.platform.startswith("darwin"):  # macOS
+            subprocess.call(["open", temp_file_path])
+
+        elif sys.platform.startswith("linux"):
+            subprocess.call(["xdg-open", temp_file_path])
+            
+        elif sys.platform.startswith("win"):
+            os.startfile(temp_file_path)
+            
+    @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Skipping tests in GitHub Actions environment")
+    def test_export_users_transformer_forbidden(self):
+        
+        user_repo= UserRepositoryMock()
+
+        extractor= DownloadUsersExtractor(repo=user_repo)
+
+        transformer= DownloadUsersTransformer(extractor=extractor)
+        
+        request = HttpRequest(body={
+            'user_from_authorizer':{
+                'id': user_repo.users[0].user_id,
+                'displayName': user_repo.users[0].name,
+                'mail': user_repo.users[0].email
+            }
+        })
+
+        response = transformer(request=request)
+
+        assert response.status_code == 403
+            
+    def test_export_users_transformer_real_repo(self):
+                    
+        datasource= TestsRdsDatasource()
+                    
+        user_repo= UserRepositoryPostgres(db_datasource=datasource)
+
+        extractor= DownloadUsersExtractor(repo=user_repo)
+
+        transformer= DownloadUsersTransformer(extractor=extractor)
+        
+        request = HttpRequest(body={
+            'user_from_authorizer':{
+                'id': '550e8400-e29b-41d4-a716-446655440001',
+                'displayName': 'Admin User',
+                'mail': 'admin@example.com'
+            }
+        })
+        
+        response = transformer(request=request)
+        
+        assert response.status_code == 200
+        
+        output= base64.b64decode(response.body['file_base64'])
 
         with tempfile.NamedTemporaryFile(delete=False, suffix = ".xlsx") as temp_file:
             temp_file.write(output)
