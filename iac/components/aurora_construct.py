be5b2718abb28e8a4b4e5c0d6be87bf51981135d
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2026-01-06 17:59:26 -0300

    reverting rds data endpoint removal as it is needed anyway

--- iac/components/aurora_construct.py
+++ iac/components/aurora_construct.py
@@ -43,7 +43,11 @@ class AuroraConstruct(Construct):
             service=ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER
         )
         
-
+        vpc.add_interface_endpoint(
+            f"RdsDataEndpoint-{stage}",
+            service=ec2.InterfaceVpcEndpointAwsService.RDS_DATA
+        )
+        
         creds = rds.Credentials.from_generated_secret("app_user", secret_name=f"/pe_mss/aurora/{stage}/credentials")
         
         db_name = "PortalEntidades_UserTable"
