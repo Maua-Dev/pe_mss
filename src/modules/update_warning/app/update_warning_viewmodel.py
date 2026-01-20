9d6617933dd129c4b7ddb76266d40b1ecd25ce01
Mateus <mateuscaxambu@gmail.com>
2026-01-20 00:06:02 -0300

    feat: test 2 🥷

--- src/modules/update_warning/app/update_warning_viewmodel.py
+++ src/modules/update_warning/app/update_warning_viewmodel.py
@@ -6,7 +6,7 @@ class UpdateWarningViewmodel:
 
     def to_dict(self):
         return {
-            "updated-warning": {
+            "updated_warning": {
                 "warning_id": self.warning.warning_id,
                 "target_role": self.warning.target_role,
                 "target_org": self.warning.target_org if self.warning.target_org else None,
