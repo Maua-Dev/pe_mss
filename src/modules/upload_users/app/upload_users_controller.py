6befc26aae01261a14967e91959bc621219336c7
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2026-01-21 13:47:32 -0300

    refactoring upload users module: added better return for comparison if users were created. fixed logic where duplicated users where going to excel. fixed logic where token was not passed to usecase. changed logic from list upload to individual request for every user. added if else clause for testing with mock insted if real module.

--- src/modules/upload_users/app/upload_users_controller.py
+++ src/modules/upload_users/app/upload_users_controller.py
@@ -36,11 +36,15 @@ class UploadUsersController:
                     fieldTypeReceived=type(file_base64).__name__
                 )
 
-            uploaded_content = self.UploadUsersUsecase(file_base64=file_base64, requester_user_id=requester_user_id, auth_token=auth_token)
+            created_users, duplicated_users = self.UploadUsersUsecase(file_base64=file_base64, requester_user_id=requester_user_id, auth_token=auth_token)
 
-            print(uploaded_content)
+            print(f"Created: {len(created_users)} users. Duplicated: {len(duplicated_users)} users.")
 
-            viewmodel = UploadUsersViewmodel(status=200 if isinstance(uploaded_content, list) else 500)
+            viewmodel = UploadUsersViewmodel(
+                status=200,
+                uploaded_users=created_users,
+                duplicated_users=duplicated_users
+            )
 
             return OK(viewmodel.to_dict())
 
--- src/modules/upload_users/app/upload_users_viewmodel.py
+++ src/modules/upload_users/app/upload_users_viewmodel.py
@@ -4,13 +4,21 @@ from src.shared.domain.enums.state_enum import STATE
 
 class UploadUsersViewmodel:
     status: int
+    uploaded_users: list[dict]
+    duplicated_users: list[dict]
 
-    def __init__(self, status: int):
+    def __init__(self, status: int, uploaded_users: list[dict] = None, duplicated_users: list[dict] = None):
         self.status = status
+        self.uploaded_users = uploaded_users if uploaded_users else []
+        self.duplicated_users = duplicated_users if duplicated_users else []
 
     def to_dict(self):
         return {
             'status': self.status,
+            'operation_for_comparison': {
+                'uploaded_users': self.uploaded_users,
+                'duplicated_users': self.duplicated_users
+            },
             'message': "the users were uploaded successfully" if self.status == 200 else "there was an error uploading the users"
         }
 
